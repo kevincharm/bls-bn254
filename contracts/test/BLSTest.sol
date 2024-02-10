ba16509250a8e8fa25f70fe84ddaddc9228a0b67
Kevin Charm <kevin@kevincharm.com>
2024-02-10 17:56:20 +0100

    add gas tests

--- contracts/test/BLSTest.sol
+++ contracts/test/BLSTest.sol
@@ -7,27 +7,35 @@ contract BLSTest {
     function test__expandMsgTo96(
         bytes memory domain,
         bytes memory message
-    ) external pure returns (bytes memory) {
-        return BLS.expandMsgTo96(domain, message);
+    ) external view returns (bytes memory expanded, uint256 gas) {
+        gas = gasleft();
+        expanded = BLS.expandMsgTo96(domain, message);
+        gas = gas - gasleft();
     }
 
     function test__hashToField(
         bytes memory domain,
         bytes memory message
-    ) external pure returns (uint256[2] memory) {
-        return BLS.hashToField(domain, message);
+    ) external view returns (uint256[2] memory p, uint256 gas) {
+        gas = gasleft();
+        p = BLS.hashToField(domain, message);
+        gas = gas - gasleft();
     }
 
     function test__mapToPointFT(
         uint256 value
-    ) external pure returns (uint256[2] memory) {
-        return BLS.mapToPoint(value);
+    ) external view returns (uint256[2] memory p, uint256 gas) {
+        gas = gasleft();
+        p = BLS.mapToPoint(value);
+        gas = gas - gasleft();
     }
 
     function test__hashToPoint(
         bytes memory domain,
         bytes memory message
-    ) external view returns (uint256[2] memory) {
-        return BLS.hashToPoint(domain, message);
+    ) external view returns (uint256[2] memory p, uint256 gas) {
+        gas = gasleft();
+        p = BLS.hashToPoint(domain, message);
+        gas = gas - gasleft();
     }
 }
--- test/BLS.spec.ts
+++ test/BLS.spec.ts
@@ -24,8 +24,9 @@ describe('BLS', () => {
         for (let i = 0; i < 10; i++) {
             const msg = crypto.randomBytes(32)
 
-            const impl = await blsTest.test__expandMsgTo96(domain, msg)
+            const [impl, gas] = await blsTest.test__expandMsgTo96(domain, msg)
             // console.log(`expandMsgTo96(${hexlify(msg)}) = ${hexlify(impl)}`)
+            // console.log(`gas: ${gas}`) // 5967
 
             // vs mcl
             const refMcl = hexlify(expandMsg(domain, msg, 96))
@@ -37,8 +38,9 @@ describe('BLS', () => {
         const domain = toUtf8Bytes('BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_')
         const msg = crypto.randomBytes(32)
 
-        const impl = await blsTest.test__hashToField(domain, msg)
+        const [impl, gas] = await blsTest.test__hashToField(domain, msg)
         // console.log(`hashToField(${hexlify(domain)}, ${hexlify(msg)}) = ${impl}`)
+        // console.log(`gas: ${gas}`) // 6491
 
         // vs mcl
         expect(impl).to.deep.eq(hashToField(domain, msg, 2))
@@ -49,11 +51,13 @@ describe('BLS', () => {
         const msg = crypto.randomBytes(32)
 
         const u = hashToField(domain, msg, 2)
-        const p0Impl = await blsTest.test__mapToPointFT(u[0])
-        const p1Impl = await blsTest.test__mapToPointFT(u[1])
+        const [p0Impl, p0Gas] = await blsTest.test__mapToPointFT(u[0])
+        const [p1Impl, p1Gas] = await blsTest.test__mapToPointFT(u[1])
 
         // console.log(`mapToPoint(${u[0]}) = ${p0Impl}`)
+        // console.log(`p0Gas: ${p0Gas}`) // ~25k
         // console.log(`mapToPoint(${u[1]}) = ${p1Impl}`)
+        // console.log(`p1Gas: ${p1Gas}`) // ~33k
 
         // vs mcl
         const p0Mcl = g1ToBig(mapToPoint('0x' + u[0].toString(16)))
@@ -64,14 +68,18 @@ describe('BLS', () => {
 
     it('correctly implements hashToPoint', async () => {
         const domain = toUtf8Bytes('BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_')
-        const msg = crypto.randomBytes(32)
 
-        const hashImpl = await blsTest.test__hashToPoint(domain, msg)
-        // console.log(`hashToPoint(${hexlify(msg)}) = ${hashImpl}`)
+        for (let i = 0; i < 10; i++) {
+            const msg = crypto.randomBytes(32)
 
-        // mcl
-        setDomain(Buffer.from(domain).toString('utf-8'))
-        const hashRef = g1ToBig(hashToPoint(hexlify(msg)))
-        expect(hashImpl).to.deep.eq(hashRef)
+            const [hashImpl, gas] = await blsTest.test__hashToPoint(domain, msg)
+            // console.log(`hashToPoint(${hexlify(msg)}) = ${hashImpl}`)
+            // console.log(`gas: ${gas}`) // ~~ min 50706, max 72506
+
+            // mcl
+            setDomain(Buffer.from(domain).toString('utf-8'))
+            const hashRef = g1ToBig(hashToPoint(hexlify(msg)))
+            expect(hashImpl).to.deep.eq(hashRef)
+        }
     })
 })
