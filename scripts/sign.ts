437782358f9ebfbaa3853a9a9efe547d9838d5fc
Kevin Charm <kevin@kevincharm.com>
2024-02-29 13:30:23 +0100

    add ability to specify custom DST in bls helper utils

--- scripts/sign.ts
+++ scripts/sign.ts
@@ -1,18 +1,27 @@
-import { toUtf8Bytes } from 'ethers'
+import { getBytes, isHexString, toUtf8Bytes } from 'ethers'
 import { BlsBn254, kyberMarshalG1, kyberMarshalG2 } from '../lib/BlsBn254'
 
-const domain = toUtf8Bytes('BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_')
+// Sign with BLS on BN254
+//
+// Usage
+//  yarn bls:sign "0xprivatekey" "message to sign" [optional DST]
+
+const DEFAULT_DOMAIN = 'BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_'
 
 async function main() {
     const bls = await BlsBn254.create()
     const _secretKey = process.argv[2] as `0x${string}`
-    const _msg = process.argv[3]
+    const msg = process.argv[2]
+    const dst = process.argv[3] || DEFAULT_DOMAIN
+    const msgBytes = isHexString(msg) ? getBytes(msg) : toUtf8Bytes(msg)
+    const dstBytes = isHexString(dst) ? getBytes(dst) : toUtf8Bytes(dst)
+    const point = bls.hashToPoint(dstBytes, msgBytes)
     const { secretKey, pubKey } = bls.createKeyPair(_secretKey)
-    const msg = bls.hashToPoint(domain, toUtf8Bytes(_msg))
-    const { signature } = bls.sign(msg, secretKey)
+    const { signature } = bls.sign(point, secretKey)
     console.log(
         JSON.stringify(
             {
+                pubKey: kyberMarshalG2(pubKey),
                 signature: kyberMarshalG1(signature),
             },
             null,
