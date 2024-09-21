ebf831af29d91755a25e1c405692f7fd59175941
Kevin Charm <kevin@kevincharm.com>
2024-09-21 20:43:15 +0200

    remove mcl, use noble-curves

--- scripts/hash.ts
+++ scripts/hash.ts
@@ -1,5 +1,5 @@
 import { getBytes, isHexString, toBeHex, toUtf8Bytes } from 'ethers'
-import { BlsBn254 } from '../lib/BlsBn254'
+import { bn254 } from '../lib/bn254'
 
 // hash-to-point
 //
@@ -12,22 +12,23 @@ import { BlsBn254 } from '../lib/BlsBn254'
 // with optional DST
 //  yarn bls:hash 0xdeadbeef "custom domain separator"
 
-const DEFAULT_DOMAIN = 'BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_' // DST used in drand BN254 for hashing to G1
+const DEFAULT_DOMAIN = 'BLS_SIG_BN254G1_XMD:KECCAK-256_SVDW_RO_NUL_' // DST used in drand BN254 for hashing to G1
 
 async function main() {
-    const bls = await BlsBn254.create()
     const msg = process.argv[2]
     const dst = process.argv[3] || DEFAULT_DOMAIN
     const msgBytes = isHexString(msg) ? getBytes(msg) : toUtf8Bytes(msg)
     const dstBytes = isHexString(dst) ? getBytes(dst) : toUtf8Bytes(dst)
-    const hash = bls.serialiseG1Point(bls.hashToPoint(dstBytes, msgBytes))
+    const { x, y } = bn254.G1.hashToCurve(msgBytes, {
+        DST: dstBytes,
+    }).toAffine()
 
     console.log(
         `G1("${dst}")`,
         JSON.stringify(
             {
-                x: toBeHex(hash[0], 32),
-                y: toBeHex(hash[1], 32),
+                x: toBeHex(x, 32),
+                y: toBeHex(y, 32),
             },
             null,
             4,
--- scripts/sign.ts
+++ scripts/sign.ts
@@ -1,28 +1,29 @@
-import { getBytes, isHexString, toUtf8Bytes } from 'ethers'
-import { BlsBn254, kyberMarshalG1, kyberMarshalG2 } from '../lib/BlsBn254'
+import { getBytes, hexlify, isHexString, toUtf8Bytes } from 'ethers'
+import { bn254 } from '../lib/bn254'
 
 // Sign with BLS on BN254
 //
 // Usage
 //  yarn bls:sign "0xprivatekey" "message to sign" [optional DST]
 
-const DEFAULT_DOMAIN = 'BLS_SIG_BN254G1_XMD:KECCAK-256_SSWU_RO_NUL_'
+const DEFAULT_DOMAIN = 'BLS_SIG_BN254G1_XMD:KECCAK-256_SVDW_RO_NUL_'
 
 async function main() {
-    const bls = await BlsBn254.create()
     const _secretKey = process.argv[2] as `0x${string}`
+    const secretKey = BigInt(_secretKey)
     const msg = process.argv[2]
     const dst = process.argv[3] || DEFAULT_DOMAIN
     const msgBytes = isHexString(msg) ? getBytes(msg) : toUtf8Bytes(msg)
     const dstBytes = isHexString(dst) ? getBytes(dst) : toUtf8Bytes(dst)
-    const point = bls.hashToPoint(dstBytes, msgBytes)
-    const { secretKey, pubKey } = bls.createKeyPair(_secretKey)
-    const { signature } = bls.sign(point, secretKey)
+    const pubKey = bn254.G2.ProjectivePoint.fromPrivateKey(secretKey).toHex()
+    const signature = bn254.signShortSignature(msgBytes, secretKey, {
+        DST: dstBytes,
+    })
     console.log(
         JSON.stringify(
             {
-                pubKey: kyberMarshalG2(pubKey),
-                signature: kyberMarshalG1(signature),
+                pubKey,
+                signature: hexlify(signature),
             },
             null,
             4,
